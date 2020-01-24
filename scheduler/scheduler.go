1467a36747144a45c35b1f95cc84e6c6ade7e480
cbahn <junco3@gmail.com>
2020-01-24 15:29:03 -0600

    added getEliminations()

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -11,17 +11,22 @@ Start    elim1    elim2    elim3+End
   |--------|--------|--------|
 */
 
+// Schedule tracks the times of the start, end, and progress
 type Schedule struct {
 	startTime            time.Time
 	endTime              time.Time
 	numberOfEliminations int
 }
 
+// Phase is an Enum
 type Phase int
 
 const (
+	// Before my linter really likes comments
 	Before Phase = iota
+	// During ..
 	During
+	// After ..
 	After
 ) // Golang Enum notation is weird
 
@@ -34,4 +39,20 @@ func (sch Schedule) getPhase() Phase {
 		return During
 	}
 	return After
+}
+
+func (sch Schedule) getEliminations() int {
+	now := time.Now()
+
+	if now.Before(sch.startTime) {
+		return 0
+	} else if now.Before(sch.endTime) {
+		// Dividing these time durations is confusing
+		// See https://stackoverflow.com/questions/54777109/dividing-a-time-duration-in-golang
+		eliminationPeriod := sch.endTime.Sub(sch.startTime) / time.Duration(sch.numberOfEliminations)
+
+		timeSinceStart := now.Sub(sch.startTime)
+		return int(timeSinceStart / eliminationPeriod)
+	}
+	return sch.numberOfEliminations
 }
--- scheduler/scheduler_test.go
+++ scheduler/scheduler_test.go
@@ -41,3 +41,31 @@ func TestPhase(t *testing.T) {
 		t.Errorf("ended.getPhase() test failed")
 	}
 }
+
+func TestGetEliminations(t *testing.T) {
+	now := time.Now()
+
+	/* each row takes the form:
+	{start hour, end hour, number eliminations, expeced value}
+	*/
+	testData := [][]int{
+		{-1, 1, 1, 0},
+		{1, 2, 5, 0},
+		{-2, -1, 5, 5},
+		{-1, 1, 5, 2},
+		{-1000, 1, 7, 6},
+		{-20, 57, 93, 24},
+	}
+
+	for i, d := range testData {
+		sch := Schedule{
+			startTime:            now.Add(time.Duration(d[0]) * time.Hour),
+			endTime:              now.Add(time.Duration(d[1]) * time.Hour),
+			numberOfEliminations: d[2],
+		}
+
+		if sch.getEliminations() != d[3] {
+			t.Errorf("Test[%d] expected %d, got %d", i, d[3], sch.getEliminations())
+		}
+	}
+}
